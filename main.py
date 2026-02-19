9ef7166d665ca59fc0a6b44bd02b15128f0ad8ca
Zsombi1089 <116731984+Zsombi1089@users.noreply.github.com>
2026-02-19 18:07:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import ctypes
+import sys
+
+fajlnev = "windows_c.dll" if sys.platform.startswith('win') else "linux_c.so"
+
+try:
+    ckonyvtar0 = ctypes.CDLL(rf"./{fajlnev}")
+except OSError as hibakod:
+    print(f"{hibakod}")
+    sys.exit(1)
+
+# Beállítjuk a függvény típusait
+ckonyvtar0.duplaz.argtypes = [ctypes.c_int]       # bemenet: egy int
+ckonyvtar0.duplaz.restype = ctypes.c_int          # visszatérési érték: int
+
+print(ckonyvtar0.duplaz(3))
